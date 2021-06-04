ff5a37834836319e7e04764da9e606ca2e2acc85
Jegrillo <75953450+Jegrillo@users.noreply.github.com>
2021-06-04 12:38:01 +0300

    Add files via upload

---
+++ Lab4/Lab4/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+        int N;
+        Double P;
+        string line;
+        public Form1()
+        {
+            InitializeComponent();
+            Answer.ReadOnly = true;
+            Text = "Тестирование диалоговых окон";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnChooseColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog dlg = new ColorDialog();
+            dlg.FullOpen = true;
+            dlg.Color = colorDialog2.Color;
+            dlg.ShowHelp = true;
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                this.BackColor = dlg.Color;
+            }
+        }
+
+        private void Text_change_Click(object sender, EventArgs e)
+        {
+            FontDialog dlg = new FontDialog();
+            dlg.ShowColor = true;
+            dlg.ShowHelp = true;
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                Answer.Font = dlg.Font;
+                Elements.Font = dlg.Font;
+            }
+        }
+
+        private void File_change_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.InitialDirectory = Application.StartupPath;
+            dlg.Filter = "txt files (*.txt)|*.txt|" +
+                " Мои файлы (Расширение не придумал)|*.xxx|" +
+                "Сборки (*.exe)|*.exe";
+            dlg.FilterIndex = 3;
+            dlg.Title = "Выбор моего файла";
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                string a = dlg.FileName;
+                StreamReader f_in = new StreamReader(a);
+                line = f_in.ReadLine();
+                N = Convert.ToInt32(line);
+                Elements.Text = N.ToString();
+
+            }
+        }
+
+       
+
+
+
+        private void Solution_Click(object sender, EventArgs e)
+        {
+            int T = 45;
+            int A = 78;
+            P = 0;
+            int m = -1;
+            int k = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                m += 2;
+                k += 2;
+                if (i%2== 0)
+                {
+                    P -= (Math.Pow(T,m) * Math.Sin(Math.Pow(A,i+1)))/(m*k);
+                }
+                else
+                {
+                    P += Math.Pow(T, m) * Math.Cos(Math.Pow(A, i + 1))/ (m * k);
+                }
+            }
+            Answer.Text = P.ToString();
+        }
+
+        private void Elements_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                N = Convert.ToInt32(Elements.Text);
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+
+        private void Answer_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ lab7/WpfApp_LR7/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_LR7
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Title = "Lr_7";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double Z=0;
+            try
+            {
+                double n = Convert.ToDouble(N.Text);
+                double k = Convert.ToDouble(K.Text);
+                double x = Convert.ToDouble(X.Text);
+                double y = Convert.ToDouble(Y.Text);
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < k; j++)
+                    {
+                        Z = (Math.Abs(Math.Pow(x, i)) + Math.Cos(y) * Math.Pow(y, j)) / ((i + 1) * j);
+                    }
+                }
+                this.Title = $" Ответ = {Z}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+    }
+}
